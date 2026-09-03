461972b3e7dface769d7965208585ffe8df73a37
cixxywuffie <cixxywuffie@users.noreply.github.com>
2026-09-03 21:13:29 +0200

    Delete user data when leave a server

--- src/main/java/com/cixtrowolf/protoseal/persistence/consent/ConsentSettingRepository.java
+++ src/main/java/com/cixtrowolf/protoseal/persistence/consent/ConsentSettingRepository.java
@@ -3,8 +3,13 @@ package com.cixtrowolf.protoseal.persistence.consent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface ConsentSettingRepository extends JpaRepository<ConsentSetting, Long> {
 
     Optional<ConsentSetting> findByGuildIdAndUserId(String guildId, String userId);
+
+    List<ConsentSetting> findByGuildIdAndOwnerUserId(String guildId, String ownerUserId);
+
+    void deleteByGuildIdAndUserId(String guildId, String userId);
 }
