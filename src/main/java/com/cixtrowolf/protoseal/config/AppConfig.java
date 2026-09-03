3b968e55c84ac0e34879025bcce63ee1e1c29e05
cixxywuffie <cixxywuffie@users.noreply.github.com>
2026-09-03 23:43:16 +0200

    Fixed intent configuration

--- src/main/java/com/cixtrowolf/protoseal/config/AppConfig.java
+++ src/main/java/com/cixtrowolf/protoseal/config/AppConfig.java
@@ -4,6 +4,8 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
+import discord4j.gateway.intent.Intent;
+import discord4j.gateway.intent.IntentSet;
 import discord4j.rest.RestClient;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    static final IntentSet ENABLED_INTENTS = IntentSet.nonPrivileged()
+            .or(IntentSet.of(Intent.GUILD_MEMBERS));
 
     private static String tokenHolder;
 
@@ -24,6 +29,7 @@ public class AppConfig {
     public GatewayDiscordClient gatewayDiscordClient() {
         return DiscordClientBuilder.create(tokenHolder).build()
                 .gateway()
+                .setEnabledIntents(ENABLED_INTENTS)
                 .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.listening("to /commands")))
                 .login()
                 .block();
